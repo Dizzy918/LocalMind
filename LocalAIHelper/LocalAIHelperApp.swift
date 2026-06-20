b53b1300534bb83ed6075614535c8b9aedaf6343
Radoslav <radoslav.s.slavov@gmail.com>
2026-06-20 11:45:44 +0300

    Initial Commit

---
+++ LocalAIHelper/LocalAIHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAIHelperApp.swift
+//  LocalAIHelper
+//
+//  Created by Radoslav Slavov on 20.06.26.
+//
+
+import SwiftUI
+
+@main
+struct LocalAIHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
